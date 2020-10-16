b0d7e8a3a46f256b7c3caa756e35bcb55639679c
Jarodavid <40429691+Jarodavid@users.noreply.github.com>
2020-10-16 20:50:31 +0800

    10/16/20

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose"); 
+const bodyParser   = require('body-parser');
 
 const companiesRouter = require('./routes/companies')
 const studentsRouter = require('./routes/students')
@@ -15,6 +16,10 @@ const db = mongoose.connection
 db.on('error',(error)=>console.error(error))
 db.once('open',()=>console.log('Connected to Database'))
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+const routes = require('../REST-API/routes/router');
+routes(app);
 
 app.listen(process.env.PORT || 3000) 
