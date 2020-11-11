2cd7fb3f0f465f9cc0190bc19bae06a228626478
Jarodavid <40429691+Jarodavid@users.noreply.github.com>
2020-11-11 22:13:41 +0800

    .

--- server.js
+++ server.js
@@ -25,17 +25,9 @@ app.use('/students',studentsRouter)
 
 app.set('view-engine', 'ejs')
   app.use(express.urlencoded({ extended: false }))
-  app.use(flash())
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  }))
-  app.use(passport.initialize())
-  app.use(passport.session())
-  app.use(methodOverride('_method'))
+ 
 
-
+ 
 mongoose.connect(process.env.DATABASE_URL,{ useUnifiedTopology: true, useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error',(error)=>console.error(error))
