cece53151891be9e6f457539a4003e69909d0717
Jarodavid <40429691+Jarodavid@users.noreply.github.com>
2020-11-11 21:39:30 +0800

    .

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+  }
 
-const express = require("express");
+const express = require('express');
 const app = express();
-const mongoose = require("mongoose"); 
+const mongoose = require('mongoose'); 
 const bodyParser   = require('body-parser');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+const router = express.Router();
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,6 +22,19 @@ const studentsRouter = require('./routes/students')
 
 app.use('/companies',companiesRouter)
 app.use('/students',studentsRouter) 
+
+app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(flash())
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  app.use(methodOverride('_method'))
+
 
 mongoose.connect(process.env.DATABASE_URL,{ useUnifiedTopology: true, useNewUrlParser: true })
 const db = mongoose.connection
