e109b83223479ad2c94a94e5c3454013fa13ba09
Jarodavid <40429691+Jarodavid@users.noreply.github.com>
2020-12-03 20:11:33 +0800

    first Update

--- server.js
+++ server.js
@@ -1,33 +1,20 @@
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-  }
+}
 
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose'); 
 const bodyParser   = require('body-parser');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
-const methodOverride = require('method-override');
-const router = express.Router();
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 const companiesRouter = require('./routes/companies')
 const studentsRouter = require('./routes/students')
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }))
 app.use('/companies',companiesRouter)
 app.use('/students',studentsRouter) 
 
-app.set('view-engine', 'ejs')
-  app.use(express.urlencoded({ extended: false }))
- 
-
- 
 mongoose.connect(process.env.DATABASE_URL,{ useUnifiedTopology: true, useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error',(error)=>console.error(error))
